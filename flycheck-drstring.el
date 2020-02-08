57d7065e820d2dca95d480b858308db8430be2b1
Daniel Martín <mardani29@yahoo.es>
2020-02-08 12:29:06 +0100

    Tweak configuration instructions

--- flycheck-drstring.el
+++ flycheck-drstring.el
@@ -12,13 +12,13 @@
 
 ;; This package adds support for linting documentation in Swift files
 ;; using DrString (https://github.com/dduan/DrString/).  To configure
-;; this package, add the following lines to your initialization file:
+;; this package, add the following line to your initialization file:
 
-;; (require 'flycheck-drstring)
 ;; (flycheck-drstring-setup)
 
 ;; DrString is a CLI tool that can be installed using your favorite
 ;; package manager.  For example, if you use Homebrew:
+
 ;; $ brew install dduan/formulae/drstring
 
 ;;; License:
